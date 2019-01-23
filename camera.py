fc9fa0ffc75c9a11c6eee9303d630f8fac66ed17
Shamoon Siddiqui <shamoon@develop.io>
2019-01-23 09:41:04 -0500

    Trying to get camera to load

---
+++ camera.py
@@ -0,0 +1,7 @@
+import cv2
+
+def open_camera(device_number=None):
+    if device_number == None:
+        return cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+    else:
+        return cv2.VideoCapture(device_number)
